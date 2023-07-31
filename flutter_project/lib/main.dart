00aa72f9481055c174c3ccf1ca26ba2e520a2b3d
João Lima <j.davi4242@gmail.com>
2023-07-31 12:00:56 -0300

    trabalhando na questão2 da aula1 de flutter

--- flutter_project/lib/main.dart
+++ flutter_project/lib/main.dart
@@ -12,11 +12,73 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('Minhas receitas')),
-        body: Center(
-          child: Text('Sem receitas ainda!'),
+        body: Column(
+          children: [
+            Center(
+              child: Text(
+                'Sobremesas',
+                style: const TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Text('Torta de Maçã',
+            style: const TextStyle(
+                  fontSize: 18,
+                ),),
+            Text('Mousse de Chocolate',
+            style: const TextStyle(
+                  fontSize: 18,
+                ),),
+            Text('Pudim de Leite Condensado',
+            style: const TextStyle(
+                  fontSize: 18,
+                ),),
+            Text(
+              'Pratos Principais',
+              style: const TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text('Frango Assado com Batatas',
+            style: const TextStyle(
+                  fontSize: 18,
+                ),),
+            Text('Espaguete a Bolonhesa',
+            style: const TextStyle(
+                  fontSize: 18,
+                ),),
+            Text('Risoto de Cogumelos',
+            style: const TextStyle(
+                  fontSize: 18,
+                ),),
+            Text(
+              'Aperitivos',
+              style: const TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text('Bolinhos de Queijo',
+            style: const TextStyle(
+                  fontSize: 18,
+                ),),
+            Text('Bruschetta de Tomate e Manjericão',
+            style: const TextStyle(
+                  fontSize: 18,
+                ),),
+            Text('Canapés de Salmão com Cream Cheese',
+            style: const TextStyle(
+                  fontSize: 18,
+                ),),
+          ],
         ),
-        floatingActionButton: FloatingActionButton(onPressed: () {},
-        child: Icon(Icons.add),),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ),
       ),
     );
   }
